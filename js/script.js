5fd3ced56adb196d47c8b5bdd4ad61d13a682f77
Igor Dejak <igordejak@MacBook-Pro.local>
2024-06-18 09:50:36 +0200

    files added

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ window.document.onload = setTimeout(function() {
     element.classList.remove('ace');
     element.classList.add('results');
 }
-, 8000 )
+, 8000 ) 
 
 window.document.onload = setTimeout(function() {
     element = document.getElementById("field-block");
